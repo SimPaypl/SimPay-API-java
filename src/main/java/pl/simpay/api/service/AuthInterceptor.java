947cd7d68a9c0fee215e1633d32a16d5d1235007
Rafał Więcek <gitzzz14@gmail.com>
2023-03-31 22:14:04 +0200

    Additional headers with informations

--- src/main/java/pl/simpay/api/service/AuthInterceptor.java
+++ src/main/java/pl/simpay/api/service/AuthInterceptor.java
@@ -12,6 +12,8 @@ class AuthInterceptor implements Interceptor {
 
     private static final String SIM_KEY_HEADER = "X-SIM-KEY";
     private static final String SIM_PASSWORD_HEADER = "X-SIM-PASSWORD";
+    private static final String SIM_VERSION_HEADER = "X-SIM-VERSION";
+    private static final String SIM_PLATFORM_HEADER = "X-SIM-PLATFORM";
 
     private final String apiKey;
     private final String simPassword;
@@ -19,7 +21,7 @@ class AuthInterceptor implements Interceptor {
     @NotNull
     @Override
     public Response intercept(@NotNull Chain chain) throws IOException {
-        var authRequest = chain.request().newBuilder().header(SIM_KEY_HEADER, apiKey).header(SIM_PASSWORD_HEADER, simPassword).build();
+        var authRequest = chain.request().newBuilder().header(SIM_KEY_HEADER, apiKey).header(SIM_PASSWORD_HEADER, simPassword).header(SIM_VERSION_HEADER, "2.1.1").header(SIM_PLATFORM_HEADER, "JAVA").build();
         return chain.proceed(authRequest);
     }
 }
